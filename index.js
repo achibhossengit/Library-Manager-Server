be1fd6e49af27bbcf7ea10ab15669401a5fe842c
achibhossengit <mail.achibhossen@gmail.com>
2025-09-19 03:32:29 +0600

    implement books related api without authentication

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = 3000;
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 // Mongodb Config
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2dlckac.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -20,8 +22,65 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // start wokring from here
+    const database = client.db("LibraryManagerDB");
+    const booksColl = database.collection("Books");
+
     app.get("/", (req, res) => {
       res.send("Library Manager Server is Running Well!");
+    });
+
+    // book related api's
+    app.get("/books", async (req, res) => {
+      try {
+        console.log(req?.header?.authorization);
+
+        const { email } = req.query;
+        let query = {};
+        if (email) {
+          query.addedBy = email;
+        }
+        const result = await booksColl.find(query).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching books:", error);
+        res.status(500).send({ message: "Internal Server Error" });
+      }
+    });
+
+    app.post("/books", async (req, res) => {
+      const newBook = req.body;
+      const result = await booksColl.insertOne(newBook);
+      res.send(result);
+    });
+
+    app.put("/books/:book_id", async (req, res) => {
+      try {
+        const { book_id } = req.params;
+        const updatedBook = { ...req.body };
+        // Remove _id if it exists
+        delete updatedBook._id;
+        const query = { _id: new ObjectId(book_id) };
+        const updateDoc = { $set: updatedBook };
+
+        const result = await booksColl.updateOne(query, updateDoc);
+        res.send(result);
+      } catch (error) {
+        console.error("Error updating book:", error);
+        res.status(500).send({ message: "Internal Server Error" });
+      }
+    });
+
+    app.delete("/books/:book_id", async (req, res) => {
+      try {
+        const { book_id } = req.params;
+
+        const query = { _id: new ObjectId(book_id) };
+        const result = await booksColl.deleteOne(query);
+        res.send(result);
+      } catch (error) {
+        console.error("Error Deleting book:", error);
+        res.status(500).send({ message: "Internal Server Error" });
+      }
     });
 
     // Send a ping to confirm a successful connection
@@ -31,7 +90,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
